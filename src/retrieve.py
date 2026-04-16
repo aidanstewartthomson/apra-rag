90bb5574039142eb46ae04086df8013633379673
Aidan Stewart-Thomson <275597918+aidanstewartthomson@users.noreply.github.com>
2026-04-16 14:17:17 +1000

    feat(retrieve): fetch top-k similar chunks

--- src/retrieve.py
+++ src/retrieve.py
@@ -1,4 +1,5 @@
-from config import EMBEDDING_MODEL
+import chromadb
+from config import CHROMA_DIR, COLLECTION_NAME, EMBEDDING_MODEL, TOP_K
 from openai import OpenAI
 
 
@@ -7,12 +8,47 @@ def embed_query(query: str, client: OpenAI) -> list[float]:
     return response.data[0].embedding
 
 
-def main() -> None:
-    client = OpenAI()
-    query = "What are the rules around replacing board members?"
-
+def retrieve_chunks(
+    query: str, collection: chromadb.Collection, client: OpenAI, n_results: int = TOP_K
+):
     query_embedding = embed_query(query, client)
-    print(query_embedding)
+    results = collection.query(query_embeddings=[query_embedding], n_results=n_results)
+
+    ids = results["ids"][0]
+    documents = results["documents"][0]
+    metadatas = results["metadatas"][0]
+    distances = results["distances"][0]
+
+    chunks = []
+
+    for id, document, metadata, distance in zip(ids, documents, metadatas, distances):
+        chunk = {
+            "id": id,
+            "title": metadata["title"],
+            "section": metadata["section"],
+            "text": document,
+            "distance": distance,
+        }
+        chunks.append(chunk)
+
+    return chunks
+
+
+def main() -> None:
+    openai_client = OpenAI()
+
+    chroma_client = chromadb.PersistentClient(CHROMA_DIR)
+    collection = chroma_client.get_collection(COLLECTION_NAME)
+
+    query = "What are the rules around replacing board members?"
+    chunks = retrieve_chunks(query, collection, openai_client)
+
+    for chunk in chunks:
+        print(f"\nID: {chunk['id']}")
+        print(f"Title: {chunk['title']}")
+        print(f"Section: {chunk['section']}")
+        print(f"Distance: {chunk['distance']}")
+        print(f"Text: {chunk['text']}")
 
 
 if __name__ == "__main__":
